b9491b59943210febb02ad662c932a38f1bd4ab5
Nevio83 <neviomichelhans9@gmail.com>
2026-01-13 22:01:23 +0100

    wuerfel3

---
+++ wuerfel/wuerfel2/Wuerfel3.java
@@ -0,0 +1,57 @@
+//Franz Hain
+//Übung: Konstruktor
+public class Wuerfel3
+{
+  //Attribute
+  private double dblSeiteA;
+   
+  //Konstruktoren
+  public Wuerfel3()     //Standardkonstruktor
+  {
+    
+  }
+
+  public Wuerfel3(double prmSeiteA)     //benutzerdefinierter Konstruktor
+  {
+    dblSeiteA=prmSeiteA;  
+  }
+      
+  //Methoden
+  public void setSeiteA(double prmSeiteA)
+  {
+    dblSeiteA=prmSeiteA;
+  }  
+
+  public double getSeite()
+  {
+    return dblSeiteA;
+  }     
+     
+  //Fläche berechnen
+  public double getFlaeche()
+  {
+    double dblFlaeche;
+    dblFlaeche=6*dblSeiteA*dblSeiteA;
+     
+    return dblFlaeche; 
+  }
+   
+  //Umfang berechnen
+  public double getKantenlaenge()
+  {
+    double dblKantenlaenge;
+    dblKantenlaenge=12*dblSeiteA;         
+      
+    return dblKantenlaenge;
+  }
+   
+  //Volumen berechnen
+  public double getVolumen()
+  {
+    double dblVolumen;
+    dblVolumen=dblSeiteA*dblSeiteA*dblSeiteA;
+      
+    return dblVolumen;
+  }
+
+}
